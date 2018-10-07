44f3760e62f197ce7e17eb3afccdfa617f86e90b
michgboxy2 <michgboxy@gmail.com>
2018-10-07 08:58:55 +0100

    schema for the user following list

---
+++ server/models/follows.js
@@ -0,0 +1,15 @@
+const mongoose =  require('mongoose'),
+      Schema   =  mongoose.Schema;
+
+
+      const followsSchema = new Schema({
+          user: {
+              type: Schema.Types.ObjectId, ref: 'user'
+          },
+          following: [{
+               type: Schema.Types.ObjectId, ref: 'user'
+          }] 
+      });
+
+
+      mongoose.model('following', followsSchema);
