dfd1268c8a4d9d47fc54b3b9e5df206be2cbe3fb
michgboxy2 <michgboxy@gmail.com>
2018-10-09 14:44:34 +0100

    mutation file

--- server/schema/mutations.js
+++ server/schema/mutations.js
@@ -2,6 +2,7 @@ const graphql = require('graphql');
 const ProfileType = require('./types/profile_types');
 const auth        = require('../services/auth');
 const utils       = require('../services/utils');
+const mongoose    = require('mongoose');
 
 
 const {
@@ -12,8 +13,9 @@ const {
 
 const userType = require('./types/user_type');
 const AuthService = require('../services/authController');
-// const ProfileType = require('./profile_types');
 const ProfileController = require('../services/profileController');
+const Profile = mongoose.model('profile');
+const followController  = require('../services/followsController');
 
 const mutation = new GraphQLObjectType({
     name: 'Mutation',
@@ -22,10 +24,15 @@ const mutation = new GraphQLObjectType({
             type: userType,
             args: {
                 username: {type: GraphQLString},
-                password: {type: GraphQLString}
+                password: {type: GraphQLString},
+                bio: {type : GraphQLString}
             },
-            resolve(parentValue, {username, password}, req){
-                return AuthService.signUp({username, password, req});
+            resolve(parentValue, {username, password, bio}, req){
+                // return AuthService.signUp({username, password, req});
+                return AuthService.signUp({username, password}).then(user => {
+                    ProfileController.AddProfile(user.id, bio);
+                    return user;
+                })
             }
         },
 
@@ -56,6 +63,18 @@ const mutation = new GraphQLObjectType({
 
                 return ProfileController.AddProfile(bio, userId);
 
+            }
+        },
+
+        follow: {
+            type : userType,
+            args: {
+                id: {type: GraphQLID}
+            },
+            resolve(parentValue, {id}, context){
+                const userId = utils.getUserId(context);
+
+                return followController.follow(userId, id);
             }
         }
     }
