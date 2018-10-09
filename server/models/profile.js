59f35ce5d6d2fd092c2b77ea27997f91e912506c
michgboxy2 <michgboxy@gmail.com>
2018-10-09 14:41:26 +0100

    profile model file

--- server/models/profile.js
+++ server/models/profile.js
@@ -1,78 +1,24 @@
-const mongoose = require('mongoose'),
-      Schema   = mongoose.Schema;
+const mongoose =  require('mongoose'),
+      Schema   =  mongoose.Schema;
+
 
       const ProfileSchema = new Schema({
-        photo : {type : String},
-        
-        user : { 
-              type : mongoose.Schema.Types.ObjectId, 
-              ref : 'user', 
-              required : "You must be logged in"
+          user : {
+              type: Schema.Types.ObjectId,
+              ref:   'user'
             },
-
-        bio : {type : String},
+          bio  : { type: String },
           
-        followers : [{ //those following me
+          following : [{
               type : Schema.Types.ObjectId, 
-              ref: "follower"
+              ref : 'user'
             }],
-          
-        following : [{ //those i'm following
-                type : Schema.Types.ObjectId, 
-                ref: "following"
-              }]
-      }, {usePushEach : true });
+         followers: [{
+            type : Schema.Types.ObjectId, 
+            ref : 'user'
+         }]
+      },{usePushEach : true});
+      
 
-// ProfileSchema.statics.followOomf = function(id, next){
-//     const Follow = mongoose.model('following');
-    
-//     return this.findById(id).then(following => {
-//         if(!following){return next(new Error("can't find user"))};
-//             const follow = new Follow({following});
-//             profile.following.push(follow);
-//             return Promise.all([follow.save(), profile.save()])
-//             .then(([follow, profile]) => profile);
-//             return profile;       
-//     }, (err) => {return next(err);})
-// }
 
-ProfileSchema.statics.unfollowOomf = function(id, next){  //following and unfollowing
-    const Following = mongoose.model('following');
-    this.following.findByIdAndRemove(id, next).then(unfollowed => {
-        if(!unfollowed){ return profile};
-        const follow = new Following({following : unfollowed});
-        profile.following.push(follow);
-        return Promise.all([follow.save(), profile.save()])
-        .then(([follow, profile]) => profile);
-        return profile;
-
-    }, (err) => {return next(err);})
-}
-
-ProfileSchema.statics.FollowMe = function(id, next){  //getting followed and unfollowed
-    const Follower = mongoose.model('follower');
-
-    this.followers.findByIdAndRemove(id).then(data => {
-        if(!data){ //not a follower
-            const follower = new Follower({follow : id});
-            this.followers.push(follower);
-            return Promise.all([profile.save(), follower.save()])
-            .then(([follower, profile]) => profile);
-            return profile;
-        }
-        return profile;
-
-    }, (err) => {return next(err);})
-}
-
-ProfileSchema.statics.getFollowers = function(id){
-    return this.findById(id)
-    .populate('followers')
-    .then(profile => profile.followers);
-}
-
-ProfileSchema.statics.getFollowing = function(id){
-    return this.findById(id)
-    .populate('following')
-    .then(profile => profile.followers);
-}
+      mongoose.model('profile', ProfileSchema);
